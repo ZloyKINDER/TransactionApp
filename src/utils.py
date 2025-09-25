8c5dbeba1205cedc957c574ecffe8bf23919f20f
Timur Gorashchenko <timyr98239718@mail.ru>
2025-09-25 20:44:56 +0300

    update utils

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,8 @@
 import datetime
+import os
+from typing import Dict, List
+
+import pandas as pd
 
 
 def get_greeting() -> str:
@@ -17,6 +21,29 @@ def get_greeting() -> str:
         massage = "Добрый вечер"
 
     return massage
+
+def read_transactions_xlsx(file_path: str) -> list[dict]:
+    """
+    Функция для считывания финансовых операций из Excel
+    """
+    xlsx_data = pd.read_excel(file_path)
+    return xlsx_data.to_dict(orient="records")
+
+
+def get_last_four(input_string:str) -> str:
+    """
+    Функция для возвращения последний четырёх символов
+    """
+    if input_string:
+        return input_string[-4:]
+    return "None"
+
+def get_cashback(total_spent: float) -> float:
+    """
+    Функция для расчёта кешбека
+    """
+    return round((total_spent / 100), 2)
+
 
 
 
